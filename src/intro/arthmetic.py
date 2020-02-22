dce656ab336aa0b82920e2af75f1d1118a75248b
jean salvi <38810299+salviosage@users.noreply.github.com>
2020-02-22 12:44:40 +0200

    Introduction (#1)
    * add arthmetic
    
    * add valiables
    
    * add type conversion

---
+++ src/intro/arthmetic.py
@@ -0,0 +1,29 @@
+# arthmetic operators 
+print((1+2-6)*8/6)
+
+#exponentiation
+print (14**2)
+
+#modulo
+print(20%3)
+
+#integer division 
+print(5//2)
+print(-9//4)
+
+# It's time to try a calculation in Python!
+
+# My electricity bills for the last three months have been $23, $32 and $64. What is the average monthly electricity bill over the three month period? Write an expression to calculate the mean, and use print() to view the result.
+
+# In this quiz you're going to do some calculations for a tiler. Two parts of a floor need tiling. One part is 9 tiles wide by 7 tiles long, the other is 5 tiles wide by 7 tiles long. Tiles come in packages of 6.
+
+# How many tiles are needed?
+# You buy 17 packages of tiles containing 6 tiles each. How many tiles will be left over?
+
+
+
+# Which of these lines of Python code are well formatted? How would you improve the readability of the codes that don't use good formatting? (Choose all that apply)
+
+print(1/4 -7*7)
+print((1+2+3) /  45)
+print((1-5)/(2+5))
---
+++ src/intro/valiable.py
@@ -0,0 +1,43 @@
+rwanda_population=159898899
+# x,y,z=3,2,5
+x=3
+y=x
+x=5
+print(y)
+
+# Only use ordinary letters, numbers and underscores in your variable names. They can’t have spaces, and need to start with a letter or underscore.
+# You can’t use reserved words or built-in identifiers
+#The pythonic way to name variables is to use all lowercase letters and underscores to separate words.
+
+my_height = 58
+my_lat = 40
+my_long = 105
+
+#instead of this
+# my height = 58
+# MYLONG = 40
+# MyLat = 105
+
+# assignment operator 
+my_height += 2
+my_lat -= 10
+
+
+# The current volume of a water reservoir (in cubic metres)
+reservoir_volume = 4.445e8
+# The amount of rainfall from a storm (in cubic metres)
+rainfall = 5e6
+
+# decrease the rainfall variable by 10% to account for runoff
+
+# add the rainfall variable to the reservoir_volume variable
+
+# increase reservoir_volume by 5% to account for stormwater that flows
+# into the reservoir in the days following the storm
+
+# decrease reservoir_volume by 5% to account for evaporation
+
+# subtract 2.5e5 cubic metres from reservoir_volume to account for water
+# that's piped to arid regions.
+
+# print the new value of the reservoir_volume variable
